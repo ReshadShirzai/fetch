6aed752fdc27b28978515e7ca203e139db1faf71
ReshadShirzai <302552943@student.rocmondriaan.nl>
2023-12-06 16:04:08 +0100

    l

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const ulList = document.querySelector("ul");
-fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
+const pokediv = document.querySelector(".gens");
+const pokecheri = document.querySelector(".cherries");
+
+fetch("https://pokeapi.co/api/v2/pokemon/?limit=25")
 .then((response) => response.json())
 .then((data) => {
     console.log("ontvangen data:", data);
@@ -10,3 +13,28 @@ fetch("https://pokeapi.co/api/v2/pokemon/?limit=10")
         ulList.innerHTML += `<li>${p.name}</li>`;
     }
 });
+
+fetch("https://pokeapi.co/api/v2/generation/?limit=10")
+.then((response) => response.json())
+.then((data) => {
+    console.log("ontvangen tweede data:", data);
+
+    const pokegames = data.results;
+    for (let i = 0; i < pokegames.length; i++) {
+        const g = pokegames[i];
+        pokediv.innerHTML += `<li>${g.name}</li>`
+    }
+});
+
+fetch("https://pokeapi.co/api/v2/berry/?limit=20")
+.then((response) => response.json())
+.then((data) => {
+    console.log("Ontvangen derde data:", data);
+
+    const pokecherries = data.results;
+    for (let i = 0; i < pokecherries.length; i++) {
+        const ch = pokecherries[i];
+
+        pokecheri.innerHTML += `<li>${ch.name}</li>`
+    }
+})
